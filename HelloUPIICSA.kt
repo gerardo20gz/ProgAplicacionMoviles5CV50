570112904c0f0e2e1476592ba38c4022302ecc21
gerardo20gz <143913633+gerardo20gz@users.noreply.github.com>
2023-09-03 18:07:59 -0600

    Add files via upload

---
+++ HelloUPIICSA.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var nombre = "Gerardo";
+    var edad = 20;
+    var estatura = 1.73;
+    //imprimir la variable a
+    var a = 1025;
+    var b = 8;
+    var c = 4;
+    c += b;
+    print("HelloUPIICSA!!! ");
+    print("Yo soy " + nombre+" mi edad es "+edad+" mi estatura es: "+estatura);
+    print("El valor de la variable a es: "+a);
+    //Imprimir con el println
+    println("El valor de la variable b es: "+b);
+    println("El valor de la variable c es: "+c);
+
+}
