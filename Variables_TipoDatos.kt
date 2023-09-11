1ada7a987da36dff3ffd8aaa9e7f16027b3ca0e5
gerardo20gz <143913633+gerardo20gz@users.noreply.github.com>
2023-09-10 23:23:23 -0600

    Add files via upload

---
+++ Variables_TipoDatos.kt
@@ -0,0 +1,16 @@
+
+ object constantes {
+    const val PI = 3.1416
+    }
+
+fun main(){
+
+    var numero: Double = 12.03645555222
+    //println(numero.toFloat())
+   
+   	val resultado  = (2 * constantes.PI )*numero
+    
+    println("El perímetro del círculo es: $area")
+    
+    
+}
