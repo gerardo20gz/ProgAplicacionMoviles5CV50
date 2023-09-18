aee5e9a5bbf217b1069b4508bdc5a1f374f77b17
gerardo20gz <143913633+gerardo20gz@users.noreply.github.com>
2023-09-17 23:02:12 -0600

    Add files via upload

---
+++ pendiente.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    //funcion de la pendiente
+    //m = (y2-y1)/(x2-x1)
+
+    val p1x = 4f
+    val p1y = 3f
+    val p2x = -3f
+    val p2y= -2f
+    var pendiente = ((p2y-p1y)/(p2x-p1x))
+    println("El resultado de la pendiente es: $pendiente")
+}
